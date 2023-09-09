0ad1850798b63b19e6e50d80c5466719931d029d
Erik Peteri <peteri.erik@gmail.com>
2023-09-09 14:38:13 +0300

    Work on the navbar

--- website/views.py
+++ website/views.py
@@ -5,3 +5,7 @@ views = Blueprint('view', __name__)
 @views.route('/')
 def home():
     return render_template("home.html")
+
+@views.route('/shopping-cart')
+def shopping_cart():
+    return render_template("base.html")
