d0e9ba6b5a9066de8b1aee84a96d81daa01cf5df
Erik Peteri <peteri.erik@gmail.com>
2023-09-12 20:25:17 +0300

    create view for individual watch

--- website/views.py
+++ website/views.py
@@ -6,10 +6,25 @@ views = Blueprint('view', __name__)
 
 @views.route('/')
 def watches():
-    result = db.session.execute(text("SELECT brand, model, price FROM watches;"))
+    result = db.session.execute(text("SELECT id, brand, model, price FROM watches;"))
     watches = result.fetchall()
     return render_template("watches.html", watches=watches)
 
 @views.route('/shopping-cart')
 def shopping_cart():
     return render_template("cart.html")
+
+@views.route('/watch/<int:id>')
+def watch_detail(id):
+    watch_id = id
+    query = db.session.execute(text("SELECT brand, model, price FROM watches WHERE id=:watch_id;"), {'watch_id': watch_id})
+    details = query.fetchone()
+
+    details_dict = {
+        'brand': details[0],
+        'model': details[1],
+        'price': details[2]
+    }
+
+    print(details_dict)
+    return render_template("watch_detail.html", details=details_dict)
