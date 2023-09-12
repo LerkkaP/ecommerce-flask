0d8e4415a32eb04d49fa2e846a8ea05619634495
Erik Peteri <peteri.erik@gmail.com>
2023-09-12 12:05:07 +0300

    define watches layout

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()  
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'dev'
+    app.config["SQLALCHEMY_DATABASE_URI"] = getenv('DATABASE_URL')
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
--- website/views.py
+++ website/views.py
@@ -1,10 +1,14 @@
 from flask import Blueprint, render_template
+from sqlalchemy.sql import text
+from . import db
 
 views = Blueprint('view', __name__)
 
 @views.route('/')
-def home():
-    return render_template("home.html")
+def watches():
+    result = db.session.execute(text("SELECT brand, model, price FROM watches;"))
+    watches = result.fetchall()
+    return render_template("watches.html", watches=watches)
 
 @views.route('/shopping-cart')
 def shopping_cart():
