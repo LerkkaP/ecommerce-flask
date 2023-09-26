08eba00169f3ec87e19c872c344420beb1d59147
Erik Peteri <peteri.erik@gmail.com>
2023-09-26 18:29:00 +0300

    add ability to delete items from cart

--- website/carts.py
+++ website/carts.py
@@ -12,10 +12,18 @@ def add_cart():
     user_id = session.get("user_id")
     
     if request.method == "POST":
-        query = ("INSERT INTO cart (watch_id, user_id, quantity) values (:watch_id, :user_id, :quantity);")
-        db.session.execute(text(query), {"watch_id": watch_id, "user_id": user_id, "quantity": quantity})
-        db.session.commit()
-        flash("Watch added to cart!")
+        count = db.session.execute(text("SELECT COUNT(*) FROM cart WHERE watch_id=:watch_id"), {'watch_id': watch_id})
+        count_result = count.fetchone()[0]
+        if count_result == 0:
+            query = ("INSERT INTO cart (watch_id, user_id, quantity) values (:watch_id, :user_id, :quantity);")
+            db.session.execute(text(query), {"watch_id": watch_id, "user_id": user_id, "quantity": quantity})
+            db.session.commit()
+            flash("Watch added to cart!")
+
+        else:
+            db.session.execute(text("UPDATE cart SET quantity = quantity + :quantity WHERE watch_id=:watch_id"), {'watch_id': watch_id, 'quantity': quantity})
+            db.session.commit()
+            flash("Item updated")
 
         return redirect(url_for('view.watch_detail', id=watch_id))
 
@@ -31,5 +39,16 @@ def shopping_cart():
     
     return render_template("cart.html", items=items)
 
+@carts.route('/deleteitem', methods=['POST'])
+def delete_item():
+    watch_id = request.form.get('watch_id')
+    quantity = request.form.get('quantity')
+    if int(quantity) == 1:
+        db.session.execute(text("DELETE FROM cart WHERE watch_id=:watch_id"), {'watch_id': watch_id})
+        db.session.commit()
+    else:
+        db.session.execute(text("UPDATE cart SET quantity = quantity - 1 WHERE watch_id=:watch_id"), {'watch_id': watch_id})
+        db.session.commit()
 
+    return redirect(url_for('carts.shopping_cart'))
 
