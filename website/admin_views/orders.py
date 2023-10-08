3ef331450ef728d7361d9d2553a8ff7f7f25281a
Erik Peteri <peteri.erik@gmail.com>
2023-10-08 21:26:57 +0300

    create tables for orders and users in admin panel

--- website/admin_views/orders.py
+++ website/admin_views/orders.py
@@ -1,8 +1,29 @@
 from flask_admin import BaseView, expose
-
 from sqlalchemy.sql import text
+
+from .. import db
 
 class Orders(BaseView):
     @expose('/')
     def index(self):
-        return self.render('admin/orders.html')
+        query = db.session.execute(text("SELECT watch_id, first_name, last_name, shipping_address, billing_address, phone_number, email, quantity, payment_method, order_date FROM orders GROUP BY id ORDER BY order_date;"))
+
+        orders = query.fetchall()
+
+        orders_list = []
+        for order in orders:
+            orders_dict = {
+                'watch_id': order[0],
+                'first_name': order[1],
+                'last_name': order[2],
+                'shipping_address': order[3],
+                'billing_address': order[4],
+                'phone_number': order[5],
+                'email': order[6],
+                'quantity': order[7],
+                'payment_method': order[8],
+                'order_date': order[9]
+            }
+            orders_list.append(orders_dict)
+
+        return self.render('admin/orders.html', orders=orders_list)
--- website/admin_views/users.py
+++ website/admin_views/users.py
@@ -1,8 +1,21 @@
 from flask_admin import BaseView, expose
-
 from sqlalchemy.sql import text
+
+from .. import db
 
 class Users(BaseView):
     @expose('/')
     def index(self):
-        return self.render('admin/users.html')
+        query = db.session.execute(text("SELECT username, privileges FROM users;"))
+
+        users = query.fetchall()
+
+        users_list = []
+        for user in users:
+            users_dict = {
+                'username': user[0],
+                'privileges': user[1],
+            }
+            users_list.append(users_dict)
+
+        return self.render('admin/users.html', users=users_list)
